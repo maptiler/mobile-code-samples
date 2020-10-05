46a2ab16a098549b3b46e0a449f7652f77e6cae2
Petr Pokorny <petr-pokorny@hotmail.com>
2020-10-05 22:10:49 +0200

    UIKit iOS app - sample app
    Initial commit

---
+++ ios-uikit/SimpleMap_UIKit/SimpleMap_UIKit/ViewController.swift
@@ -0,0 +1,31 @@
+import Mapbox
+import UIKit
+
+class ViewController: UIViewController, MGLMapViewDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let mapTilerKey = getMapTilerkey()
+
+        title = "Simple Map"
+        let styleURL = URL(string: "https://api.maptiler.com/maps/streets/style.json?key=\(mapTilerKey)")
+        let mapView = MGLMapView(frame: view.bounds, styleURL: styleURL)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.delegate = self
+        mapView.logoView.isHidden = true
+        // Set the map’s center coordinate and zoom level.
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 47.127757, longitude: 8.579139), zoomLevel: 10, animated: false)
+        view.addSubview(mapView)
+    }
+
+    func getMapTilerkey() -> String {
+        guard let mapTilerKey = UIApplication.mapTilerKey else {
+            preconditionFailure("Failed to read MapTiler key from info.plist")
+        }
+        let result: ComparisonResult = mapTilerKey.compare("placeholder", options: NSString.CompareOptions.caseInsensitive, range: nil, locale: nil)
+        if result == .orderedSame {
+            preconditionFailure("Please enter correct MapTiler key in info.plist[MapTilerKey] property")
+        }
+        return mapTilerKey
+    }
+}
