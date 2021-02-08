45a5a3f5beb2800a57e9220603517b2cb676c64e
Petr Pokorny <petr-pokorny@hotmail.com>
2021-02-08 20:38:50 +0100

    Renaming starter projects and adding new app with drawer menu for iOS

---
+++ samples/index.md
@@ -0,0 +1,3 @@
+## MapLibre SDK
+
+### Welcome
---
+++ samples/ios-swiftui/MapLibreExamples/MapLibreExamples/MapLibreExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapLibreExamplesApp.swift
+//  MapLibreExamples
+//
+//  Created by Petr Pokorny on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct MapLibreExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
